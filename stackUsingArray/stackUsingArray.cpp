aae644cdb35c8b59a0d3607d829c89c9719ac958
Jodinurf <jodinurfarkhani04@gmail.com>
2023-06-16 15:44:25 +0700

    membuat method pop dimana jika data kosong maka menampilkan stack is empty cannot pop, jika tidak maka akan menampilkan dan menghapus element pop dengan cara top decrement

--- stackUsingArray/stackUsingArray.cpp
+++ stackUsingArray/stackUsingArray.cpp
@@ -27,4 +27,15 @@ public:
 
 		return element;
 	}
+
+	void pop() {
+		if (empty()) { //step 1
+			cout << "\nStack is empty. Cannot pop." << endl; //1.a
+			return; //1.b
+		}
+
+		cout << "\nThe popped element is : " << stack_array[top] << endl; //step 3
+		top--; //step 3 decrement
+	}
+
 };
